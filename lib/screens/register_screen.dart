1e2a29fadaac4a0a670d00206cefebc34af5f8e5
Maricarmen <ecoaquiracu@unsa.edu.pe>
2024-01-07 12:02:07 -0500

    Creación del proyecto flutter y onboarding screen

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:garden_mate/utils/constants.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+        backgroundColor: Constants.primaryColor,
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Primary color
+  static var primaryColor = const Color(0xff296e48);
+  static var blackColor = Colors.black54;
+
+  //Onboarding texts
+  static var titleOne = "Garden Mate";
+  static var descriptionOne = "Aplicativo móvil orientado al cuidado de plantas mediantes dispositivos IoT.";
+  static var titleTwo = "Controla la humedad de las plantas";
+  static var descriptionTwo = "Garden Mate controla la humedad de las plantas.";
+  static var titleThree = "Controla la temperatura de tu plantas";
+  static var descriptionThree = "Garden Mate controla la temperatura de las plantas.";
+}
